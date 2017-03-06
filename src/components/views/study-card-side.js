941bd3c61e0229013c5bc885047bdee8f2feef6d
Travis CI <ikandrius@gmail.com>
2017-03-06 15:21:41 +0200

    Tweak set study css

--- src/components/views/study-card-side.js
+++ src/components/views/study-card-side.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function CardSide({ side, content, getSideElement, revealBack }) {
     function renderSide(side, content) {
         return (
-            <div className="study-side" ref={elem => getSideElement(elem, side)}>
+            <div className="study-side-content-container" ref={elem => getSideElement(elem, side)}>
                 <p className="study-side-content">{content}</p>
             </div>
         );
@@ -12,10 +12,12 @@ export default function CardSide({ side, content, getSideElement, revealBack }) 
     return (
         <div className="study-side-container">
             <div className="side-name">{side}</div>
-            {content ?
-                renderSide(side, content) :
-                <button className="btn study-reveal-btn" onClick={revealBack}>Reveal</button>
-            }
+            <div className="study-side">
+                {content ?
+                    renderSide(side, content) :
+                    <button className="btn study-reveal-btn" onClick={revealBack}>Reveal</button>
+                }
+            </div>
         </div>
     );
 }
