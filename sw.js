7a2e076e190b713a68dceb8cb5a21c986c9ff875
Travis CI <ikandrius@gmail.com>
2017-05-19 15:00:37 +0000

    Deploy to GitHub Pages

---
+++ sw.js
@@ -0,0 +1,32 @@
+const cacheName = "v1";
+const toCache = ["./index.html", "./main.css", "./main.js"];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => cache.addAll(toCache))
+        .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(self.clients.claim());
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.map(key => {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+                return key;
+            }));
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => response || fetch(event.request))
+        .catch(() => caches.match("index.html"))
+    );
+});
