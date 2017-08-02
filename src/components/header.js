36d652af6e53e5f965c341ef6127ada7f67a6239
Andrius Kvedaras <ikandrius@gmail.com>
2017-08-02 15:51:39 +0300

    Fix unable to proceed to higher round in standard study mode

--- src/components/header.js
+++ src/components/header.js
@@ -10,10 +10,10 @@ export default function Header() {
                         <NavLink to="/" exact className="header-link" activeClassName="active">Home</NavLink>
                     </li>
                     <li className="header-nav-item">
-                        <NavLink to="/flashcards/create" className="header-link" activeClassName="active">Create</NavLink>
+                        <NavLink to="/flashcards" exact className="header-link" activeClassName="active">Flashcards</NavLink>
                     </li>
                     <li className="header-nav-item">
-                        <NavLink to="/flashcards" exact className="header-link" activeClassName="active">Flashcards</NavLink>
+                        <NavLink to="/flashcards/create" className="header-link" activeClassName="active">Create</NavLink>
                     </li>
                     <li className="header-nav-item">
                         <NavLink to="/settings" className="header-link" activeClassName="active">Settings</NavLink>
