b20fff0a544af90428076313fb551424eee9da88
Andrius Kvedaras <ikandrius@gmail.com>
2018-09-26 18:37:46 +0300

    Replace header home button

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import Icon from "./icon";
 
 export default function Header() {
     return (
         <header className="header">
             <nav className="header-nav">
+                <Link to="/" className="header-logo-link">
+                    <Icon name="cards" className="header-logo-icon"/>
+                    <span>NimusCards</span>
+                </Link>
                 <ul className="header-nav-items">
-                    <li className="header-nav-item">
-                        <NavLink to="/" exact className="header-link" activeClassName="active">Home</NavLink>
-                    </li>
                     <li className="header-nav-item">
                         <NavLink to="/decks/create" className="header-link" activeClassName="active">Create</NavLink>
                     </li>
