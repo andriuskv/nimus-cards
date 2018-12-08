9afc2cf89440168384bd32d12573367de3eae537
Andrius Kvedaras <ikandrius@gmail.com>
2018-12-08 15:08:46 +0200

    Add ability to expand study card image

--- src/components/decks/deck-removal-dialog.js
+++ src/components/decks/deck-removal-dialog.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function DeckRemovalDialog({ deckTitle, removeDeck, cancelRemoval }) {
     return (
-        <div className="modal-container">
+        <div className="mask">
             <div className="modal deck-dialog-box">
                 <h3 className="deck-dialog-box-title">Are you sure you want to remove <b>{deckTitle}</b> deck?</h3>
                 <div className="deck-dialog-box-btns">
